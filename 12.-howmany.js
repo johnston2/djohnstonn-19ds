9a0c25be1ca5b8b3f9bed134025f22dcfc0802d1
johnston2 <51388064+johnston2@users.noreply.github.com>
2019-09-13 17:44:31 -0500

    Update 12.-howmany.js

--- 12.-howmany.js
+++ 12.-howmany.js
@@ -1,18 +1,18 @@
-var array = [3,2,1];
+var array = [5,5,7,8,6,5];
 
 function howmany(x){
     var x = x.sort(function(a, b){return a-b});
-    var iguales = 0;
+    var repetido = 0;
     for(c1 = 0; c1 < x.length; c1++){
         if(x[c1]==x[c1+1]){
             for(c2 = c1+1; c2 < x.length; c2++){
-                if(x[c1]==x[c2]) iguales++;
+                if(x[c1]==x[c2]) repetido++;
             }
         }
-        c1= c1+iguales;
+        c1= c1+repetido;
 
     }
-    return iguales;
+    return repetido;
 }
 
 howmany(array);
